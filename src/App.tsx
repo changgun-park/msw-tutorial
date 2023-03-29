515fa224055fb5d8f385b0e70019e29450d9e4ce
changgun-park <pcg0537@gmail.com>
2023-03-29 17:27:17 +0900

    feat: add upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 interface Post {
@@ -9,22 +9,73 @@ interface Post {
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
-  axios
-    .get("api/blogs")
-    .then((res) => {
-      setPosts(res.data);
-    })
-    .catch(console.log);
+  useEffect(() => {
+    axios
+      .get("api/blogs")
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch(console.log);
+  }, []);
+
+  const handleUpload = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    axios
+      .post("api/blogs/new", {
+        title,
+        body,
+      })
+      .then(console.log)
+      .catch(console.log)
+      .finally(() => {
+        setBody("");
+        setTitle("");
+      });
+
+    axios
+      .get("api/blogs")
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch(console.log);
+  };
 
   return (
     <div>
-      {posts.map((post) => (
-        <section key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </section>
-      ))}
+      <section>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </section>
+      <br />
+      <section>
+        <h2>New Project</h2>
+        <form onSubmit={handleUpload}>
+          <label htmlFor="title">Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            id="title"
+          />
+          <label htmlFor="body">Body</label>
+          <input
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            name="body"
+            type="text"
+            id="body"
+          />
+          <button type="submit">upload</button>
+        </form>
+      </section>
     </div>
   );
 }
